2c0e014e114ebd3f7b5a3f86b8c7d07cf646372a
Kvrolix <up2024896@myport.ac.uk>
2023-03-02 13:13:47 +0000

    Class Student updated

--- flutter_designs/lib/Student.dart
+++ flutter_designs/lib/Student.dart
@@ -1,3 +1,39 @@
-import 'package:flutter_designs/Person.dart';
+class Student {
+  late String _name;
+  late String _surname;
+  late String _email;
+  late String _addressLineOne;
+  late String _addressLineTwo;
+  late String _city;
+  late String _county;
+  late String _postCode;
 
-class Student extends Person {}
+  late DateTime _dateOfBirth;
+  late int _countryCode;
+  late int _phoneNumber;
+
+  Student(
+      String name,
+      String surname,
+      String email,
+      String addressLineOne,
+      String addressLineTwo,
+      String city,
+      String county,
+      String postCode,
+      DateTime dateOfBirth,
+      int countryCode,
+      int phoneNumber) {
+    _name = name;
+    _surname = surname;
+    _email = email;
+    _addressLineOne = addressLineOne;
+    _addressLineTwo = addressLineTwo;
+    _city = city;
+    _county = county;
+    _postCode = postCode;
+    _dateOfBirth = dateOfBirth;
+    _countryCode = countryCode;
+    _phoneNumber = phoneNumber;
+  }
+}
