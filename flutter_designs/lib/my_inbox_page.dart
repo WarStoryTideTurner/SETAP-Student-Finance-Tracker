5b85c0ad399221fd899dc56fbe64d70a9e766f48
Diggydave123 <114661329+Diggydave123@users.noreply.github.com>
2023-03-30 13:52:45 +0100

    adding inbox code

--- flutter_designs/lib/my_inbox_page.dart
+++ flutter_designs/lib/my_inbox_page.dart
@@ -1,20 +1,199 @@
 import 'package:flutter/material.dart';
 
+class Email {
+  final String sender;
+  final String subject;
+  final String message;
+
+  bool selected = false;
+
+  Email({
+    required this.sender,
+    required this.subject,
+    required this.message,
+  });
+}
+
+//Widget that handles stuff to do with the user's inbox
+//Coded by David with a little help from ChatGPT
+//**Needs to be changed to be more personal and less AI generated before submission
+
 class MyInboxPage extends StatefulWidget {
-  const MyInboxPage ({super.key});
+  const MyInboxPage({Key? key}) : super(key: key);
 
   @override
   State<MyInboxPage> createState() => _MyInboxPageState();
 }
 
 class _MyInboxPageState extends State<MyInboxPage> {
+  final List<Email> emails = [
+    Email(
+      sender: 'john.doe@example.com',
+      subject: 'Meeting',
+      message: 'Hey, are you available for a meeting at 2 PM?',
+    ),
+    Email(
+      sender: 'jane.doe@example.com',
+      subject: 'Task assigned',
+      message: 'You have been assigned a new task, please complete it by EOD.',
+    ),
+    Email(
+      sender: 'bob.smith@example.com',
+      subject: 'Reminder',
+      message: 'Just a reminder that our team meeting is tomorrow at 10 AM.',
+    ),
+    Email(
+      sender: 'steve.smith@example.com',
+      subject: 'Reminder',
+      message: 'Just a reminder that our team meeting is tomorrow at 10 AM.',
+    ),
+    Email(
+      sender: 'jenny.smith@example.com',
+      subject: 'Reminder',
+      message: 'Just a reminder that our team meeting is tomorrow at 10 AM.',
+    ),
+    Email(
+      sender: 'bob.smith@example.com',
+      subject: 'Reminder',
+      message: 'Just a reminder that our team meeting is tomorrow at 10 AM.',
+    ),
+    Email(
+      sender: 'bob.smith@example.com',
+      subject: 'Reminder',
+      message: 'Just a reminder that our team meeting is tomorrow at 10 AM.',
+    ),
+    Email(
+      sender: 'bob.smith@example.com',
+      subject: 'Reminder',
+      message: 'Just a reminder that our team meeting is tomorrow at 10 AM.',
+    ),
+    Email(
+      sender: 'bob.smith@example.com',
+      subject: 'Reminder',
+      message: 'Just a reminder that our team meeting is tomorrow at 10 AM.',
+    ),
+    Email(
+      sender: 'bob.smith@example.com',
+      subject: 'Reminder',
+      message: 'Just a reminder that our team meeting is tomorrow at 10 AM.',
+    ),
+    Email(
+      sender: 'bob.smith@example.com',
+      subject: 'Reminder',
+      message: 'Just a reminder that our team meeting is tomorrow at 10 AM.',
+    ),
+    Email(
+      sender: 'bob.smith@example.com',
+      subject: 'Reminder',
+      message: 'Just a reminder that our team meeting is tomorrow at 10 AM.',
+    ),
+    Email(
+      sender: 'bob.smith@example.com',
+      subject: 'Reminder',
+      message: 'Just a reminder that our team meeting is tomorrow at 10 AM.',
+    ),
+    // Add more emails here...
+  ];
+
+  List<Email> selectedEmails = [];
+
+  void toggleEmailSelected(Email email) {
+    setState(() {
+      email.selected = !email.selected;
+      if (email.selected) {
+        selectedEmails.add(email);
+      } else {
+        selectedEmails.remove(email);
+      }
+    });
+  }
+
+  void deleteSelectedEmails() {
+    setState(() {
+      selectedEmails.forEach((email) {
+        emails.remove(email);
+      });
+      selectedEmails.clear();
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: const Text(
-        'Inbox Page',
-      )
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(selectedEmails.isEmpty ? 'Inbox' : '${selectedEmails.length} selected'),
+        actions: [
+          if (selectedEmails.isNotEmpty)
+            IconButton(
+              icon: const Icon(Icons.delete),
+              onPressed: deleteSelectedEmails,
+            ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ListView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: emails.length,
+              itemBuilder: (context, index) {
+                final email = emails[index];
+                return ListTile(
+                  leading: Checkbox(
+                    value: email.selected,
+                    onChanged: (value) => toggleEmailSelected(email),
+                  ),
+                  title: Text(email.subject),
+                  subtitle: Text(email.sender),
+                  onTap: () {
+                    // Navigate to email details page
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => EmailDetailsPage(email: email),
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class EmailDetailsPage extends StatelessWidget {
+  final Email email;
+
+  const EmailDetailsPage({Key? key, required this.email}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(email.subject),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'From: ${email.sender}',
+              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              email.message,
+              style: const TextStyle(fontSize: 16),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
