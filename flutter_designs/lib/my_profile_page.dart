40480d0e31b6046e426b31a57b36bdb2130e7874
jmdesrosi <36333469+jmdesrosi@users.noreply.github.com>
2023-05-03 18:31:42 +0100

    Removed unnecessary files.
    Removed "simple_template_idea" and accidental main.dart. Also made slight changes to my currently incomplete profile page.

--- flutter_designs/lib/my_profile_page.dart
+++ flutter_designs/lib/my_profile_page.dart
@@ -8,31 +8,23 @@ class MyProfilePage extends StatefulWidget {
 }
 
 class _MyProfilePageState extends State<MyProfilePage> {
+  final _formKey = GlobalKey<FormState>();
+
+  double sectionWidth = 250.0;
+  double sectionHeight = 250.0;
+
   String text = "Edit Profile";
   String text2 = "Share Profile";
 
-  bool alternate = false;
-  bool alternate2 = false;
+  bool alternate = true;
+  bool alternate2 = true;
 
-  void _onEditPressed() {
-    setState(() {
-      if (alternate) {
-        text = "Pressed!";
-        alternate = false;
-      }
-      else {
-        text = "Edit Profile";
-        alternate = true;
-      }
-    });
-  }
   void _onSharePressed() {
     setState(() {
       if (alternate2) {
         text2 = "Pressed!";
         alternate2 = false;
-      }
-      else {
+      } else {
         text2 = "Share Profile";
         alternate2 = true;
       }
@@ -41,90 +33,182 @@ class _MyProfilePageState extends State<MyProfilePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const SizedBox(
-          height: 10,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
+    return Scaffold(
+        appBar: AppBar(title: const Text("My Profile")),
+        body: Center(
+            child: Column(
           children: [
-            const CircleAvatar(
-              backgroundImage: AssetImage('assets/images/pfp1.jpg'),
-              radius: 50
-            ),
-            const SizedBox(
-              width: 10,
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: const [
-                Text(
-                  'Name',                
+            Container(
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                  color: Colors.white,
                 ),
-                Text(
-                  'Email',
-                ),
-                Text(
-                  'Phone Number',
-                ),
-              ],
-            )
-          ]
-        ),
-        Column(
-          children: [
-            const SizedBox(
-              height: 10,
-            ),
-            SizedBox(
-              height: 25,
-              width: 120,
-              child: TextButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.resolveWith((states) {
-                    return Colors.black;
-                  })
-                ),
-                onPressed: _onEditPressed,
-                child: Text (
-                  text
-                ),
+                margin: const EdgeInsets.all(20.0),
+                child: SizedBox(
+                    width: sectionWidth,
+                    child: Wrap(children: [
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Column(children: [
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              const CircleAvatar(
+                                  backgroundImage:
+                                      AssetImage('assets/images/pfp1.jpg'),
+                                  radius: 50),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: const [
+                                  Text(
+                                    'Name',
+                                  ),
+                                  Text(
+                                    'Email',
+                                  ),
+                                  Text(
+                                    'Phone Number',
+                                  ),
+                                ],
+                              ),
+                            ]),
+                        Column(
+                          children: [
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            SizedBox(
+                              height: 25,
+                              width: 120,
+                              child: TextButton(
+                                style: ButtonStyle(backgroundColor:
+                                    MaterialStateProperty.resolveWith((states) {
+                                  return Colors.black;
+                                })),
+                                onPressed: () {
+                                  showDialog(
+                                      barrierDismissible: false,
+                                      context: context,
+                                      builder: (BuildContext context) {
+                                        return AlertDialog(
+                                          content: Stack(
+                                            children: <Widget>[
+                                              Form(
+                                                key: _formKey,
+                                                child: Column(
+                                                  crossAxisAlignment:
+                                                      CrossAxisAlignment.start,
+                                                  mainAxisSize:
+                                                      MainAxisSize.min,
+                                                  children: <Widget>[
+                                                    TextButton(
+                                                      onPressed: () {
+                                                        Navigator.of(context)
+                                                            .pop();
+                                                      },
+                                                      child: const CircleAvatar(
+                                                        backgroundColor:
+                                                            Colors.red,
+                                                        child:
+                                                            Icon(Icons.close),
+                                                      ),
+                                                    ),
+                                                    Padding(
+                                                      padding:
+                                                          const EdgeInsets.all(
+                                                              8.0),
+                                                      child: TextFormField(
+                                                        decoration:
+                                                            const InputDecoration(
+                                                                hintText:
+                                                                    "Name"),
+                                                      ),
+                                                    ),
+                                                    Padding(
+                                                      padding:
+                                                          const EdgeInsets.all(
+                                                              8.0),
+                                                      child: TextFormField(
+                                                          decoration:
+                                                              const InputDecoration(
+                                                                  hintText:
+                                                                      "Email")),
+                                                    ),
+                                                    Padding(
+                                                      padding:
+                                                          const EdgeInsets.all(
+                                                              8.0),
+                                                      child: ElevatedButton(
+                                                        child: const Text(
+                                                            "Submit"),
+                                                        onPressed: () {
+                                                          if (_formKey
+                                                              .currentState!
+                                                              .validate()) {
+                                                            _formKey
+                                                                .currentState!
+                                                                .save();
+                                                          }
+                                                        },
+                                                      ),
+                                                    )
+                                                  ],
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        );
+                                      });
+                                },
+                                child: Text(text),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            SizedBox(
+                              height: 25,
+                              width: 120,
+                              child: TextButton(
+                                  style: ButtonStyle(backgroundColor:
+                                      MaterialStateProperty.resolveWith(
+                                          (states) {
+                                    return Colors.black;
+                                  })),
+                                  onPressed: _onSharePressed,
+                                  child: Text(text2)),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                          ],
+                        ),
+                      ]),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                    ]))),
+            Container(
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                color: Colors.white,
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            SizedBox(
-              height: 25,
-              width: 120,
-              child: TextButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.resolveWith((states) {
-                    return Colors.black;
-                  })
-                ),
-                onPressed: _onSharePressed,
-                child: Text (
-                  text2
-                )
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Column(
-              children: const [
-                Text (
-                  'Current House'
-                )
-              ],
+              margin: const EdgeInsets.all(20.0),
+              child: SizedBox(
+                  width: sectionWidth,
+                  child: Wrap(
+                    children: const [Text('Current House')],
+                  )),
             )
           ],
-        ),
-      ]
-    );
+        )));
   }
 }
