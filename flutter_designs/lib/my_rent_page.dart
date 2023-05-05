5f582258c79c852573a1c4176103da38ee89080a
Kvrolix <up2024896@myport.ac.uk>
2023-05-06 00:44:22 +0100

    Update on my_rent half of the code done

--- flutter_designs/lib/my_rent_page.dart
+++ flutter_designs/lib/my_rent_page.dart
@@ -7,57 +7,178 @@ class MyRentPage extends StatefulWidget {
   State<MyRentPage> createState() => _MyRentPageState();
 }
 
+//
 class _MyRentPageState extends State<MyRentPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("House Profile"),
+      ),
+      endDrawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            Container(
+              height: 50,
+              child: const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(20),
+                      bottomRight: Radius.circular(10)),
+                ),
+                child: Center(
+                  child: Text(
+                    "Header",
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text("Item 1"),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text("Item 2"),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text("Item 3"),
+              onTap: () {},
+            )
+          ],
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Center(
+            child: CircleAvatar(
+              radius: 80,
+              backgroundImage: AssetImage('assets/images/house1.jpeg'),
+            ),
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "123 Perfect Street",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  const Text(
+                    "Portsmouth, PO4 21S",
+                    style: TextStyle(fontSize: 16),
+                  )
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(height: 16),
+          Container(
+            padding: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 209, 204, 204),
+              borderRadius: BorderRadius.circular(8),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 1,
+                  blurRadius: 3,
+                  offset: const Offset(0, 3),
+                )
+              ],
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "House mates",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 16),
+                SizedBox(
+                  height: 120,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      _createRoomMatesProfiles(
+                          'assets/images/profilePictures/person1.jpg'),
+                      _createRoomMatesProfiles(
+                          'assets/images/profilePictures/person2.jpg'),
+                      _createRoomMatesProfiles(
+                          'assets/images/profilePictures/person3.jpg'),
+                      _createRoomMatesProfiles(
+                          'assets/images/profilePictures/person4.jpg'),
+                      _createRoomMatesProfiles(
+                          'assets/images/profilePictures/person5.jpg'),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
-      // appBar: AppBar(
-      //   title: const Text('House Profile'),
-      // ),
-      // endDrawer: Drawer(
-      //   child: ListView(
-      //     padding: EdgeInsets.zero,
-      //     children: <Widget>[
-      //       Container(
-      //         height: 50,
-      //         child: const DrawerHeader(
-      //           decoration: BoxDecoration(
-      //             color: Colors.blue,
-      //             borderRadius: BorderRadius.only(
-      //               bottomLeft: Radius.circular(20),
-      //               bottomRight: Radius.circular(20),
-      //               ),
-      //             ),
-      //             child: Center(
-      //                 child: Text(
-      //               "Header",
-      //               style: TextStyle(
-      //                   fontSize: 18,
-      //                   fontWeight: FontWeight.bold,
-      //                   fontFamily: "Rubik"),
-      //             )),
-      //           ),
-      //         ),
-      //       ),
-      //       ListTile(
-      //         title: Text("Item 1"),
-      //         onTap: () {
-      //           //Functionality
-      //         },
-      //       ),
-      //       ListTile(
-      //         title: Text("Item 2"),
-      //         onTap: () {
-      //           //Functionality
-      //         },
-      //       ),
-      //       ListTile(
-      //         title: Text("Item 3"),
-      //         onTap: () {
-      //           //Functionality
-      //         },
+// appBar: AppBar(
+//   title: const Text('House Profile'),
+// ),
+// endDrawer: Drawer(
+//   child: ListView(
+//     padding: EdgeInsets.zero,
+//     children: <Widget>[
+//       Container(
+//         height: 50,
+//         child: const DrawerHeader(
+//           decoration: BoxDecoration(
+//             color: Colors.blue,
+//             borderRadius: BorderRadius.only(
+//               bottomLeft: Radius.circular(20),
+//               bottomRight: Radius.circular(20),
+//               ),
+//             ),
+//             child: Center(
+//                 child: Text(
+//               "Header",
+//               style: TextStyle(
+//                   fontSize: 18,
+//                   fontWeight: FontWeight.bold,
+//                   fontFamily: "Rubik"),
+//             )),
+//           ),
+//         ),
+//       ),
+//       ListTile(
+//         title: Text("Item 1"),
+//         onTap: () {
+//           //Functionality
+//         },
+//       ),
+//       ListTile(
+//         title: Text("Item 2"),
+//         onTap: () {
+//           //Functionality
+//         },
+//       ),
+//       ListTile(
+//         title: Text("Item 3"),
+//         onTap: () {
+//           //Functionality
+//         },
 //             )
 //           ],
 //         ),
@@ -265,12 +386,12 @@ class _MyRentPageState extends State<MyRentPage> {
 //   }
 // }
 
-// Widget _createRoomMatesProfiles(String imagePath) {
-//   return Padding(
-//     padding: const EdgeInsets.only(right: 16),
-//     child: CircleAvatar(
-//       radius: 30,
-//       backgroundImage: AssetImage(imagePath),
-//     ),
-//   );
-// }
+Widget _createRoomMatesProfiles(String imagePath) {
+  return Padding(
+    padding: const EdgeInsets.only(right: 16),
+    child: CircleAvatar(
+      radius: 30,
+      backgroundImage: AssetImage(imagePath),
+    ),
+  );
+}
